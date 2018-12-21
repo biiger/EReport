9a98f8e76d8997105b42314453c48e0da52ccc7f
Cutiesz <nerimor@hotmail.com>
2018-12-21 18:48:25 +0700

    Commit Register Module

---
+++ app/src/main/java/com/arsoft/mobile/ereport/module/register/CreateAccountActivity.kt
@@ -0,0 +1,41 @@
+package com.arsoft.mobile.ereport.module.register
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.arsoft.mobile.ereport.R
+import kotlinx.android.synthetic.main.activity_create_account.*
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        activity_create_account__bt_next.setOnClickListener {
+            val email = activity_create_account__et_email.text.toString()
+            val pin = activity_create_account__et_pin.text.toString()
+            val pinConfirm = activity_create_account__et_pin_confirm.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "กรุณากรอกอีเมล์", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            if (pin.isEmpty()) {
+                Toast.makeText(this, "กรุณากรอกรหัส PIN", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            if (pinConfirm.isEmpty()) {
+                Toast.makeText(this, "กรุณายืนยันรหัส PIN", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            if (!pin.equals(pinConfirm)) {
+                Toast.makeText(this, "รหัส PIN ไม่ตรงกัน", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            startActivity(Intent(this@CreateAccountActivity, CreateAccountDetailsActivity::class.java))
+        }
+    }
+}
